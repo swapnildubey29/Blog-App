3b509171fbe2f58714546cc43541bd313e73e9a8
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-23 16:15:35 +0530

    Admin functions

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./server/config/db')
+const { isActiveRoute } = require('./server/helpers/routeHelpers')
 const app = express()
 
 // configration
@@ -9,6 +14,7 @@ app.use(express.static('public'))
 app.use(expressLayout)
 app.set('layout', './layouts/main')
 app.set('view engine', 'ejs')
+
 app.use('/', require('./server/routes/main'))
 app.use('/', require('./server/routes/admin'))
 
@@ -16,8 +22,22 @@ app.use('/', require('./server/routes/admin'))
 // Connect to DB
 connectDB()
 
-app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+  }));
+
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cookieParser())
+app.use(methodOverride('_method'))
+
+app.locals.isActiveRoute = isActiveRoute; 
 
 
 // Listen
