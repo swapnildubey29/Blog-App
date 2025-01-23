b33e5865108cfb2af6b788bec98e712e33824378
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-23 15:04:42 +0530

    Database Setup

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const connectDB = require('./server/config/db')
 const app = express()
 
 // configration
 app.use(express.static('public'))
 app.use(expressLayout)
+
+// Connect to DB
+connectDB()
+
 app.set('layout', './layouts/main')
 app.set('view engine', 'ejs')
-
 app.use('/', require('./server/routes/main'))
 
 
