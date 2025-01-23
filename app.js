c1dbdacaef1d5a612d0b648247f8609b78825441
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-23 13:05:25 +0530

    Initialize Project

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const app = express()
+
+// configration
+app.use(express.static('public'))
+app.use(expressLayout)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+app.use('/', require('./server/routes/main'))
+
+
+
+
+
+// Listen
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is listen at ${PORT}`)
+})
