8564593b6b86cfed26de106315a7d1ab77e497ea
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-23 15:51:57 +0530

    Api for Getting blog by Id

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
+// GET Home
 router.get("/", async (req, res) => {
   const locals = {
     title: "Blog App",
@@ -34,6 +35,31 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET Post by Id
+router.get('/post/:id', async (req, res) => {
+    try {
+      let slug = req.params.id;
+  
+      const data = await Post.findById({ _id: slug });
+  
+      const locals = {
+        title: data.title,
+        description: " Blog created with NodeJs, Express & MongoDb.",
+      }
+  
+      res.render('post', { 
+        locals,
+        data,
+        currentRoute: `/post/${slug}`
+      });
+    } catch (error) {
+      console.log(error);
+    } 
+  });
+  
+
+
+// GET About
 router.get("/about", (req, res) => {
   res.render("about", {
     currentRoute: "/about",
