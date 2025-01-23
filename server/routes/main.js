1943f84517c8fc59f8cf6dcad9f6f9f9b723e3a3
swapnildubey29 <swapnildubey3636@gmail.com>
2025-01-23 15:44:15 +0530

    Creating App to add and get blogs

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,15 +1,43 @@
-const express = require('express')
-const route = express.Router()
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
 
-route.get('/', (req,res) => {
-    res.render('index')
-})
+router.get("/", async (req, res) => {
+  const locals = {
+    title: "Blog App",
+    description: "Blog App created with NodeJs, Express & MongoDb.",
+  };
+  try {
+    let perPage = 10;
+    let page = req.query.page || 1;
 
-route.get('/about', (req,res) => {
-    res.render('about')
-})
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
 
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
 
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      currentRoute: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get("/about", (req, res) => {
+  res.render("about", {
+    currentRoute: "/about",
+  });
+});
 
-module.exports = route;
+module.exports = router;
